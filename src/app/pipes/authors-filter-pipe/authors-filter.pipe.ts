68671e65825488c02fc2ec59ecd1490886184c09
Simona Pericleanu <simona.pericleanu@yahoo.com>
2017-08-23 13:57:21 +0300

    Search authors by name and by nationality

--- src/app/pipes/authors-filter-pipe/authors-filter.pipe.ts
+++ src/app/pipes/authors-filter-pipe/authors-filter.pipe.ts
@@ -1,17 +1,20 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'authorsFilter'
+    name: 'authorsFilter'
 })
 export class AuthorsFilterPipe implements PipeTransform {
 
-  transform(authors: any, term: any): any[] {
-    if (term === undefined) {
-      return authors;
+    transform(authors: any, term: string, name: string, nationality: string): any[] {
+        if (term === undefined) {
+            return authors;
+        }
+        return authors.filter(function (author) {
+            if (author.name.toLowerCase().includes(term.toLowerCase()) || author.nationality.toLowerCase().includes(term.toLowerCase())) {
+                return true;
+            }
+        })
     }
-    return authors.filter(function(author) {
-      return author.name.toLowerCase().includes(term.toLowerCase());
-    })
-  }
-
 }
+
+
